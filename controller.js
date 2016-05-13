5424922dda917f33b962293d44985a5823d49bc0
Anton Ilyin <AntonLlyin@landsend.com>
2016-05-13 18:26:23 +0300

    some commit

--- controller.js
+++ controller.js
@@ -1,12 +1,31 @@
 (function (){
   angular
     .module('angularNotifications', [])
+    .service('userNotificationsService', function (){
+      return {
+        addNotifications: addNotifications
+      }
+
+      /**
+      * 
+      */
+      function addNotifications(){
+
+      }
+    })
     .controller('NotificaitonsController', ['$scope', function ($scope){
       var vm = this;
 
-      vm.text = 'Hello!';
+      vm.notificationsTestObjectsWithOneType = [{ message: ['test1', 'test2', 'test3'], type: 'information'}];
+      vm.notificationsTestObjectsWithManyTypes = [ { message: ['test1', 'test2'], type: 'information'},
+                                                   { message: 'JustString', type: 'error'}];
+      vm.notificationsTestJustArray = ['test1', 'test2', 'test3'];
+
     }])
     .directive('userNotifications', ['$window', function ($window){
+      /* Every displayed notification will be hidden after 4000 ms or the value from attributes*/
+      var TIME_CLOSE = 4000;
+
       return {
         template: '<div class="user-notifications">lol' +
                       '<ul><li ng-repeat="item in notifications">{{item}}</li></ul>' +
@@ -14,6 +33,11 @@
         restrict: 'AE',
         scope: {
           notifications: '='
+        },
+        link: function (scope, el, attr){
+          //scope.timeClose = parseInt(attr.timeClose, 10) || TIME_CLOSE;
+
+
         }
       }
     }]);
