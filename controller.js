7a850d1c9c98caad75487108f127e84e1494d589
aailyin <antonaleksei.ilyin@gmail.com>
2016-05-15 00:52:35 +0300

    Continue working

--- controller.js
+++ controller.js
@@ -1,22 +1,110 @@
 (function (){
   angular
     .module('angularNotifications', [])
-    .service('userNotificationsService', function (){
+    .service('userNotificationsService', ['$timeout', function ($timeout){
+      var _notifications = [],
+          _notifications_timeout = [],
+          INFO_TYPE = 'info',
+          WARN_TYPE = 'warning',
+          ERR_TYPE = 'error';
+
       return {
-        addNotifications: addNotifications
+        addNotifications: addNotifications,
+        addNotificationWithoutType: addNotificationWithoutType,
+        closeNotificationById: closeNotificationById,
+        closeNotificationByType: closeNotificationByType,
+        closeAllNotifications: closeAllNotifications,
+      };
+
+      /**
+      * Add one notification......
+      */
+      function addNotification(notification){
+        if(angular.isUndefined(notification) || notification == null || !angular.isObject(notifications)){
+          return null;
+        }
+
+        
       }
 
       /**
       * 
       */
-      function addNotifications(){
+      function addNotifications(notifications){
+        var ids = [], closedTypes = [];
+
+        if(angular.isUndefined(notifications) || notifications == null || !angular.isArray(notifications)){
+          return ids;
+        }
+
+        if(angular.isArray(notifications)){
+          notifications.forEach(function (notification){
+
+            if(notification.isReplace && notification.type){
+              if(closedTypes.indexOf(notification.type) === -1){
+                closeNotificationByType(notification.type);
+                closedTypes.push(notification.type);
+              }
+            }
+
+            if(angular.isArray(notification.message)){
+              notification.message.forEach(function (message){
+                ids.push(pushNotification(message, notification));
+              });
+            } else {
+              ids.push(pushNotification(notification.message, notification));
+            }
+          });
+        }
+
+        return ids;
+      }
+
+      function pushNotification(message, notification){
+        var id = Date.now(),
+            promise = notification.isStatic ? null : createTimeout(id),
+            type = notification.type || INFO_TYPE;
+
+        _notifications.push({
+          id: id,
+          message: message,
+          type: type
+        });
+        _notifications_timeout.push({
+          id: id,
+          type: type,
+          promise: promise
+        });
+        return id;
+      }
+
+      function createTimeout(id){
+        return $setTimeout(function() {
+          closeNotificationById(id);
+        }, TIMEOUT_TIME);
+      }
+
+
+      function addNotificationWithoutType(notification){
 
       }
-    })
+
+      function closeNotificationById(id){
+
+      }
+
+      function closeNotificationByType(type){
+
+      }
+
+      function closeAllNotifications(){
+
+      }
+    }])
     .controller('NotificaitonsController', ['$scope', function ($scope){
       var vm = this;
 
-      vm.notificationsTestObjectsWithOneType = [{ message: ['test1', 'test2', 'test3'], type: 'information'}];
+      vm.notificationsTestObjectsWithOneType = { message: ['test1', 'test2', 'test3'], type: 'information'};
       vm.notificationsTestObjectsWithManyTypes = [ { message: ['test1', 'test2'], type: 'information'},
                                                    { message: 'JustString', type: 'error'}];
       vm.notificationsTestJustArray = ['test1', 'test2', 'test3'];
@@ -24,10 +112,10 @@
     }])
     .directive('userNotifications', ['$window', function ($window){
       /* Every displayed notification will be hidden after 4000 ms or the value from attributes*/
-      var TIME_CLOSE = 4000;
+      var TIMEOUT_TIME = 4000;
 
       return {
-        template: '<div class="user-notifications">lol' +
+        template: '<div class="user-notifications">' +
                       '<ul><li ng-repeat="item in notifications">{{item}}</li></ul>' +
                   '</div>',
         restrict: 'AE',
@@ -35,10 +123,10 @@
           notifications: '='
         },
         link: function (scope, el, attr){
-          //scope.timeClose = parseInt(attr.timeClose, 10) || TIME_CLOSE;
+          //scope.timeClose = parseInt(attr.timeoutTime, 10) || TIMEOUT_TIME;
 
 
         }
-      }
+      };
     }]);
 })();
